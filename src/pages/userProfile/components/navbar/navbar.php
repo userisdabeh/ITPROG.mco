c337cd50f59002bf1e72040b647e5db563c0469b
olv <olvjar@gmail.com>
2025-08-03 17:43:17 +0800

    feat: remove redundant navbar, just use userHeader for consistency

--- src/pages/userProfile/components/navbar/navbar.php
+++
@@ -1,7 +0,0 @@
-<nav class="navbar">
-  <a href="../home/home.php" class="back-link">&larr; Back to Home</a>
-  <div class="brand">
-    <span class="brand-icon">&#10084;</span>
-    <span class="brand-name">Wonderpets</span>
-  </div>
-</nav>
